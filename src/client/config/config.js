659d068c8eabcee819bfd2da2a6897860350ae15
Grzegorz Wasiak <g.wasiak@gmail.com>
2015-09-26 20:31:56 +0200

    client configuration minor adjustment

--- src/client/config/config.js
+++ src/client/config/config.js
@@ -1,6 +1,6 @@
 export default {
     "api": {
-        "baseUrl": "//localhost:8080/api",
+        "baseUrl": "//" + window.location.host + "/api",
         "resources": {
             "allegro": {
                 "costimize": "/allegro/costimize"
