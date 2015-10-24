1a4936369d54726c9c420572091658ad4c5fc6bd
Grzegorz Wasiak <g.wasiak@gmail.com>
2015-10-24 10:33:46 +0200

    injector refactoring & test suite

--- src/shared/components/injector/injector.js
+++ src/shared/components/injector/injector.js
@@ -1,4 +1,6 @@
-import _ from "lodash";
+const ERROR_CODE_REINSTANTIATION = 1;
+const ERROR_CODE_DEPENDENCY_LOOP = 2;
+const ERROR_CODE_NO_FACTORY = 3;
 
 class Injector {
     constructor() {
@@ -7,24 +9,51 @@ class Injector {
 
     set(name, instance) {
         let injectable = this._injectables[name] || {};
+
         injectable.instance = instance;
         this._injectables[name] = injectable;
     }
 
     get(name) {
         let injectable = this._injectables[name] || {};
-        injectable.instance || (injectable.factory && (this.instantiate(name, injectable.factory)));
-        return injectable.instance;
+        let instance = injectable.instance;
+
+        if (!instance) {
+            instance = this.instantiate(name);
+        }
+
+        return instance;
     }
 
     register(name, factory) {
         this._injectables[name] = {factory};
     }
 
-    instantiate(name, factory) {
-        let dependencies = factory.$inject || [];
-        let instance = factory.apply(null, _.map(dependencies, (dependency) => this.get(dependency)));
+    instantiate(name) {
+        let injectable = this._injectables[name] || {};
+
+        if (injectable.instance) {
+            throw new Error(`Injector error ${ERROR_CODE_REINSTANTIATION}: Attepting to instantiate already instantiated injectable (${name}).`);
+        }
+
+        if (injectable.instantiated && !injectable.instance) {
+            throw new Error(`Injector error ${ERROR_CODE_DEPENDENCY_LOOP}: Circular dependency chain (${name}).`);
+        }
+
+        let factory = injectable.factory;
+        if (!factory) {
+            throw new Error(`Injector error ${ERROR_CODE_NO_FACTORY}: Attepting to instantiate not registered injectable (${name}).`);
+        }
+
+        injectable.instantiated = true;
+
+        let names = factory.$inject || [];
+        let dependencies = names.map((name) => this.get(name));
+        let instance = factory(...dependencies);
+
         this.set(name, instance);
+
+        return instance;
     }
 }
 
