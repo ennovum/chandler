61f0c5bfe446a73f5ab9569bf52b545fe9d97256
Grzegorz Wasiak <grzegorz.wasiak@contractors.roche.com>
2016-05-12 23:25:51 +0200

    tagger module

---
+++ src/shared/components/tagger/tagger.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+
+let TAGSLUG_SPLIT_REGEXP = /\s+/;
+
+function parseTags(tagslug) {
+    if (_.isArray(tagslug)) {
+        return tagslug;
+    }
+    if (_.isString(tagslug)) {
+        return tagslug.split(TAGSLUG_SPLIT_REGEXP);
+    }
+
+    return [];
+}
+
+function matchTags(tagslug1, tagslug2) {
+    let tags1 = parseTags(tagslug1);
+    let tags2 = parseTags(tagslug2);
+
+    for (let i = 0, l = tags1.length; i < l; i++) {
+        for (let j = 0, k = tags2.length; j < k; j++) {
+            if (tags1[i] === tags2[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+const tagger = {parseTags, matchTags}
+
+export default tagger;
+export {tagger, parseTags, matchTags};
