5e5ae7cdc51c31f393ba02cfb7345c7a2e00ef20
Grzegorz Wasiak <g.wasiak@gmail.com>
2016-05-22 11:25:57 +0200

    minor cleanup

--- src/client/config/config.js
+++ src/client/config/config.js
@@ -1,6 +1,5 @@
 let config = {
     "api": {
-        "baseUrl": "//" + window.location.host + "/api",
         "resources": {
             "allegro": {
                 "listing": (query, page) => "//" + window.location.host + `/api/allegro-proxy/listing?query=${query}&page=${page}`,
