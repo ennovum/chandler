af956b577d9e1c26602a31b07d417aeed418a755
Grzegorz Wasiak <g.wasiak@gmail.com>
2016-05-22 13:14:51 +0200

    prices summary in costimizer

--- src/shared/components/allegro-costimizer/allegro-costimizer.js
+++ src/shared/components/allegro-costimizer/allegro-costimizer.js
@@ -7,15 +7,26 @@ class AllegroCostimizer {
         let results = _.map(commonSellerIds, (commonSellerId) => {
             let seller = _.find(searchSets[0].items, (item) => item.seller.id === commonSellerId).seller;
             let id = seller.id;
-            let offers = _.map(searchSets, (searchSet) => ({
-                query: searchSet.query,
-                items: _.filter(searchSet.items, (item) => item.seller.id === commonSellerId)
-            }));
+            let offers = _.map(searchSets, (searchSet) => {
+                let query = searchSet.query;
+                let items = _.filter(searchSet.items, (item) => item.seller.id === commonSellerId);
+                let prices = {
+                    minimum: _.min(items, (item) => item.price).price,
+                    maximum: _.max(items, (item) => item.price).price,
+                    average: this._sanitizePrice(_.sum(items, (item) => item.price) / items.length)
+                };
+
+                return {query, items, prices};
+            });
 
             return {id, seller, offers};
         });
 
         return Promise.resolve(results);
+    }
+
+    _sanitizePrice(rawPrice) {
+        return Number(Number(rawPrice).toFixed(2));
     }
 }
 
