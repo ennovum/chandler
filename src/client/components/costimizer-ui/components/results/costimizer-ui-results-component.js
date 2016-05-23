7ea5499bbcae6b9bf17e1e6934f8727fb5420459
Grzegorz Wasiak <g.wasiak@gmail.com>
2016-05-24 01:22:43 +0200

    costimizer results redesigned

--- src/client/components/costimizer-ui/components/results/costimizer-ui-results-component.js
+++ src/client/components/costimizer-ui/components/results/costimizer-ui-results-component.js
@@ -41,73 +41,52 @@ class CostimizerUiResultsComponent {
 }
 
 const template = `
-    <div class="cards results-cards" ng-if="ctrl.results.length">
-        <div class="card" ng-class="{'expanded-card': ctrl.isSelectedResult(result)}"
-            ng-repeat="result in ctrl.sortedResults"
-        >
-            <div class="clickable card-head"
-                ng-click="ctrl.toggleResult(result)"
-            >
+    <table class="grid results-items-grid" ng-if="ctrl.results">
+        <tr class="grid-row">
+            <td class="grid-head grid-label">
+            </td>
+            <td class="grid-head" ng-repeat="query in ctrl.queries track by $index">
+                "{{query}}"
+            </td>
+        </tr>
+        <tr class="grid-row" ng-repeat="result in ctrl.sortedResults">
+            <td class="grid-head grid-label">
                 <a ng-href="{{result.seller.url}}" target="_blank">{{result.seller.name}}</a>
-            </div>
-            <div class="clickable card-body card-raw-body"
+            </td>
+            <td class="clickable grid-cell"
+                ng-repeat="offer in result.offers track by $index"
                 ng-click="ctrl.toggleResult(result)"
-                ng-if="!ctrl.isSelectedResult(result)"
             >
-                <table class="grid naked-grid compact-grid results-summary-grid">
-                    <tr class="grid-row" ng-repeat="offer in result.offers track by $index">
-                        <td class="grid-head">
-                            "{{offer.query}}"
-                        </td>
-                        <td class="grid-cell">
-                            <div>
-                                <span ng-if="offer.items.length === 1">1 offer</span>
-                                <span ng-if="offer.items.length > 1">{{offer.items.length}} offers</span>
-                            </div>
-                            <div ng-repeat="price in offer.prices">
-                                <span ng-if="price.minimum === price.maximum">
-                                    <price value="price.minimum" currency="price.currency"></price>
-                                </span>
-                                <span ng-if="price.minimum !== price.maximum">
-                                    <price value="price.minimum"></price> - <price value="price.maximum" currency="price.currency"></price>
-                                </span>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="card-body card-raw-body"
-                ng-if="ctrl.isSelectedResult(result)"
-            >
-                <table class="grid naked-grid results-items-grid">
-                    <tr class="grid-row">
-                        <td class="grid-head" ng-repeat="query in ctrl.queries track by $index">
-                            "{{query}}"
-                        </td>
-                    </tr>
-                    <tr class="grid-row">
-                        <td class="grid-cell" ng-repeat="offer in result.offers track by $index">
-                            <ol>
-                                <li class="results-offer" ng-repeat="item in offer.items track by $index">
-                                    <div>
-                                        <a ng-href="{{item.url}}" target="_blank">{{item.title}}</a>
-                                    </div>
-                                    <div>
-                                        <price value="item.price.value" currency="item.price.currency"></price>
-                                    </div>
-                                </li>
-                            </ol>
-                        </td>
-                    </tr>
-                    <tr class="grid-row" ng-if="!ctrl.results.length">
-                        <td class="grid-cell" colspan="{{1 + ctrl.queries.length}}">
-                            <span>No results</span>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
+                <div>
+                    <span ng-if="offer.items.length === 1">1 offer</span>
+                    <span ng-if="offer.items.length > 1">{{offer.items.length}} offers</span>
+                </div>
+                <div ng-repeat="price in offer.prices">
+                    <span ng-if="price.minimum === price.maximum">
+                        <price value="price.minimum" currency="price.currency"></price>
+                    </span>
+                    <span ng-if="price.minimum !== price.maximum">
+                        <price value="price.minimum"></price> - <price value="price.maximum" currency="price.currency"></price>
+                    </span>
+                </div>
+                <ol class="results-offers" ng-class="{'visible': ctrl.isSelectedResult(result)}">
+                    <li class="results-offer" ng-repeat="item in offer.items track by $index">
+                        <div>
+                            <a ng-href="{{item.url}}" target="_blank">{{item.title}}</a>
+                        </div>
+                        <div>
+                            <price value="item.price.value" currency="item.price.currency"></price>
+                        </div>
+                    </li>
+                </ol>
+            </td>
+        </tr>
+        <tr class="grid-row" ng-if="!ctrl.results.length">
+            <td class="grid-cell" colspan="{{1 + ctrl.queries.length}}">
+                <span>No results</span>
+            </td>
+        </tr>
+    </table>
 `;
 
 const controller = (...args) => new CostimizerUiResultsComponent(...args);
