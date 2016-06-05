38f1ae14349a6e0dfa409c9d63a6b8dbf706d72c
Grzegorz Wasiak <g.wasiak@gmail.com>
2016-06-06 00:21:06 +0200

    sale results combined

--- src/shared/components/sale/sale.js
+++ src/shared/components/sale/sale.js
@@ -8,7 +8,30 @@ class Sale {
 	}
 
 	sipSale(queries, handleSip) {
-		return this._allegroSale.sipSale(queries, handleSip);
+		let allegroResults = [];
+		let ceneoResults = [];
+		let results;
+
+		let allegroPromise = this._allegroSale.sipSale(queries, (results) => {
+			allegroResults = results;
+			results = [].concat(allegroResults, ceneoResults);
+			handleSip(results);
+		});
+		let ceneoPromise = this._ceneoSale.sipSale(queries, (results) => {
+			ceneoResults = results;
+			results = [].concat(allegroResults, ceneoResults);
+			handleSip(results);
+		});
+
+		let promise = Promise.all([allegroPromise, ceneoPromise])
+			.then(() => results);
+
+		promise.abort = () => {
+			allegroPromise.abort();
+			ceneoPromise.abort();
+		};
+
+		return promise;
 	}
 }
 
