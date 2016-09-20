5d24f0e6f392f6fdb31e9f997665086ad6ae37d5
Grzegorz Wasiak <g.wasiak@gmail.com>
2016-09-20 21:04:00 +0200

    fetcher timeout & attempts options

--- src/shared/modules/fetcher/fetcher.js
+++ src/shared/modules/fetcher/fetcher.js
@@ -1,34 +1,72 @@
+import get from 'lodash/get';
+import assign from 'lodash/assign';
 import fetch from 'fetch';
 
+const TIMEOUT_DEFAULT = 5000;
+const ATTEMPTS_DEFAULT = 2;
+
+const TIMEOUT_STATUS = 408;
+const TIMEOUT_STATUS_TEXT = 'Request Timeout';
+
 class Fetcher {
-    fetchText(url) {
-        return fetch(url).then((response) => {
-            let status = response.status;
-            let statusText = response.statusText;
+    fetch(url, options) {
+        const timeout = +get(options, 'timeout', TIMEOUT_DEFAULT);
+        const attempts = +get(options, 'attempts', ATTEMPTS_DEFAULT);
 
-            if (status !== 200) {
-                return response.text().then((responseText) => {
-                    throw new Error({status, statusText, responseText});
-                });
-            }
+        return new Promise((resolve, reject) => {
+            let timeoutRef = setTimeout(() => {
+                if (attempts > 0) {
+                    const attempts = attempts - 1;
+                    const options = assign(options, {timeout, attempts});
 
-            return response.text();
+                    resolve(fetch(url, options));
+                }
+                else {
+                    const status = TIMEOUT_STATUS;
+                    const statusText = TIMEOUT_STATUS_TEXT;
+                    const responseText = null;
+
+                    reject({status, statusText, responseText});
+                }
+
+                timeoutRef = null;
+            }, timeout);
+
+            this.request(url, options).then((response) => {
+                timeoutRef && clearTimeout(timeoutRef);
+                timeoutRef = null;
+
+                resolve(response);
+            }, (error) => {
+                timeoutRef && clearTimeout(timeoutRef);
+                timeoutRef = null;
+
+                reject(error);
+            });
         });
     }
 
-    fetchJSON(url) {
-        return fetch(url).then((response) => {
-            let status = response.status;
-            let statusText = response.statusText;
+    request(url, options) {
+        return new Promise((resolve, reject) => {
+            fetch(url, options).then((response) => {
+                const status = response.status;
+                const statusText = response.statusText;
 
-            if (status !== 200) {
-                return response.text().then((responseText) => {
-                    throw new Error({status, statusText, responseText});
-                });
-            }
+                if (status !== 200) {
+                    reject(response.text().then((responseText) => ({status, statusText, responseText})));
+                }
 
-            return response.json();
+                resolve(response);
+            });
         });
+    }
+
+    fetchText(url, options) {
+        return this.fetch(url, options).then((response) => response.text());
+    }
+
+    fetchJSON(url, options) {
+        return this.fetch(url, options).then((response) => response.json());
     }
 }
 
